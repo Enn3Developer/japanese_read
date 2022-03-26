0608171cf17807f80808e1c865c720b3a124f18e
Enn3Developer <arturpeshko39@gmail.com>
2022-03-26 15:41:50 +0100

    Added support for katakana and kanji Fixed a typo in the kana version of momotaro Started writing the kanji version of momotaro

--- src/lib.rs
+++ src/lib.rs
@@ -20,33 +20,62 @@ macro_rules! stop {
 #[folder = "japanese_texts"]
 struct Asset;
 
+#[derive(Clone, Copy)]
 pub enum Char {
     Hiragana,
     Katakana,
+    Kanji,
 }
 
-impl Char {
-    fn type_to_str(&self) -> &str {
-        match self {
-            Char::Hiragana => "hiragana",
-            Char::Katakana => "katakana",
-        }
-    }
-}
-
-pub fn read_random_file(file_type: &Char) -> String {
+pub fn read_random_file_kana() -> String {
     // Read a random file given the file_type
     let mut entries = Vec::new();
     for file in Asset::iter() {
-        if file.contains(file_type.type_to_str()) {
+        if file.contains("hiragana") {
             entries.push(file);
         }
     }
     assert!(!entries.is_empty());
     entries.shuffle(&mut rand::thread_rng());
     // if there aren't any file then the program should crash before we try
-    // to access a non-existent address (`entries[0]`)
-    String::from_utf8(Asset::get(&entries[0]).unwrap().as_ref().to_vec()).unwrap()
+    // to access a non-existent address (`entries.first()`)
+    String::from_utf8(
+        Asset::get(&entries.first().unwrap())
+            .unwrap()
+            .as_ref()
+            .to_vec(),
+    )
+        .unwrap()
+}
+
+pub fn read_random_file_kanji() -> (String, String) {
+    // Read a random file given the file_type
+    let mut entries = Vec::new();
+    for file in Asset::iter() {
+        if file.contains("kanji") {
+            entries.push(file);
+        }
+    }
+    assert!(!entries.is_empty());
+    entries.shuffle(&mut rand::thread_rng());
+    // if there aren't any file then the program should crash before we try
+    // to access a non-existent address (`entries.first()`)
+    (
+        String::from_utf8(
+            Asset::get(&entries.first().unwrap())
+                .unwrap()
+                .as_ref()
+                .to_vec(),
+        )
+            .unwrap(),
+        String::from_utf8(
+            Asset::get(&entries.first().unwrap().replace("kanji", "hiragana"))
+                .unwrap()
+                .as_ref()
+                .to_vec(),
+        )
+            .unwrap(),
+    )
 }
 
 pub fn report_error(user_input: &str, expected: &str) {
@@ -101,11 +130,23 @@ fn find_differences(vec_user: &[char], vec_expected: &[char]) -> Vec<usize> {
     let mut diff: Vec<usize> = vec![];
     let differ = Differ::new(vec_expected, vec_user);
     for span in differ.spans() {
-        if let Tag::Replace = span.tag {
-            for i in span.b_start..span.b_end {
-                diff.push(i);
+        match span.tag {
+            Tag::Equal => {},
+            _ => {
+                // Tags: Insert, Delete, Replace
+                for i in span.b_start..span.b_end {
+                    diff.push(i);
+                }
             }
+            // Tag::Insert => ,
+            // Tag::Delete => ,
+            // Tag::Replace => ,
         }
+        // if let Tag::Replace = span.tag {
+        //     for i in span.b_start..span.b_end {
+        //         diff.push(i);
+        //     }
+        // }
     }
 
     diff
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,17 @@ use std::process::exit;
 use wana_kana::to_hiragana::to_hiragana;
 use wana_kana::to_katakana::to_katakana;
 
-use japanese_read::{Char, read_random_file, report_error, stop};
+use japanese_read::{Char, read_random_file_kana, read_random_file_kanji, report_error, stop};
 
-fn run(file_type: Char) {
-    let file = read_random_file(&file_type);
+fn run_kana(file_type: Char) {
+    let file = read_random_file_kana();
     for line in file.lines() {
         let mut user_input = String::new();
-        println!("{}", line);
+        match file_type {
+            Char::Hiragana => println!("{}", line),
+            Char::Katakana => println!("{}", to_katakana(line)),
+            _ => {}
+        }
         // read the user input
         io::stdin()
             .read_line(&mut user_input)
@@ -20,11 +24,32 @@ fn run(file_type: Char) {
             // if the user wants to exit, let him do this
             stop!();
         }
-        match file_type {
-            // translate the user input to what he chose
-            Char::Hiragana => user_input = to_hiragana(&user_input),
-            Char::Katakana => user_input = to_katakana(&user_input),
+        user_input = to_hiragana(&user_input);
+        if user_input != line {
+            // if the two periods are not the same, then there is an error (from the user)
+            // we need to report this error
+            println!();
+            report_error(&user_input, line);
         }
+        println!();
+    }
+}
+
+fn run_kanji() {
+    let files = read_random_file_kanji();
+    for line in files.1.lines() {
+        let mut user_input = String::new();
+        println!("{}", files.0.lines().next().unwrap());
+        // read the user input
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Cannot read line");
+        user_input.pop(); // delete the new-line character (`\n`)
+        if user_input == "exit" {
+            // if the user wants to exit, let him do this
+            stop!();
+        }
+        user_input = to_hiragana(&user_input);
         if user_input != line {
             // if the two periods are not the same, then there is an error (from the user)
             // we need to report this error
@@ -38,9 +63,9 @@ fn run(file_type: Char) {
 fn main() {
     // This is a workaround to enable terminal colors in Windows
     #[cfg(windows)]
-    colored::control::set_virtual_terminal(true);
+    colored::control::set_virtual_terminal(true).unwrap();
     loop {
-        println!("Choose:\n0. Exit\n1. Hiragana\n2. Katakana");
+        println!("Choose:\n0. Exit\n1. Hiragana\n2. Katakana\n3. Kanji");
         // Read the choice
         let mut choice = String::new();
         io::stdin().read_line(&mut choice).unwrap();
@@ -50,12 +75,17 @@ fn main() {
             0 => stop!(),
             1 => Char::Hiragana,
             2 => Char::Katakana,
+            3 => Char::Kanji,
             _ => {
                 println!("Invalid choice");
                 continue;
             }
         };
         // if the choice is valid, pass `file_type` with his ownership (we don't need it anymore)
-        run(file_type);
+        match file_type {
+            Char::Hiragana => run_kana(file_type),
+            Char::Katakana => run_kana(file_type),
+            Char::Kanji => run_kanji()
+        }
     }
 }
